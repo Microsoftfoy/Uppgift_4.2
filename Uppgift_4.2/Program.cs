fca106b9fadf2150fa8ae2ca819182d01d8d2443
ercr07001 <ercr07001@E-PW05CGR0>
2024-12-11 13:43:58 +0100

    Add project files.

---
+++ Uppgift_4.2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNumber = int.MinValue;
+            bool continueInput = true;
+
+            while (continueInput)
+            {
+                Console.WriteLine("Skriv in ett heltal:");
+                int number = int.Parse(Console.ReadLine());
+
+                if (number > maxNumber)
+                {
+                    maxNumber = number;
+                }
+
+                Console.WriteLine("Vill du skriva in ett till tal? (ja/nej)");
+                string response = Console.ReadLine().ToLower();
+
+                if (response != "ja")
+                {
+                    continueInput = false;
+                }
+            }
+
+            Console.WriteLine($"Det största talet som du skrev in är: {maxNumber}");
+
+        }
+
+    }
+}
+
